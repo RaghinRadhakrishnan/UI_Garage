dc06f0987202cf2f64c4d32f1b2794cbf02cb022
Raghin Radhakrishnan <raghir13@in.ibm.com>
2019-08-21 11:48:01 +0530

    Updates

---
+++ precache-manifest.6b1ba64e8bd2fd68ef13365c76aaad9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ce6e0fe099ed98b43e650a8f3c29b6",
+    "url": "/UI_Garage/index.html"
+  },
+  {
+    "revision": "07bc3676927496005140",
+    "url": "/UI_Garage/static/css/main.1cab23f7.chunk.css"
+  },
+  {
+    "revision": "c4ceb834d83ce0d6fa9f",
+    "url": "/UI_Garage/static/js/2.9ec0eb60.chunk.js"
+  },
+  {
+    "revision": "07bc3676927496005140",
+    "url": "/UI_Garage/static/js/main.b1e2eb10.chunk.js"
+  },
+  {
+    "revision": "be67213f0a8b2385cb02",
+    "url": "/UI_Garage/static/js/runtime~main.9e2e42f6.js"
+  }
+]);
